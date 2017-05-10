f0d64f74dbee683a8fa0720fec02f93482e9d963
Joseph Lyons <josephlyons@Josephs-MBP-15.PK5001Z>
2017-05-09 23:16:43 -0400

    Added FrequencyValues class

---
+++ Source/FrequencyValues.cpp
@@ -0,0 +1,49 @@
+//
+//  FrequencyValues.cpp
+//  The Lyons' Den Delay Time Calculator
+//
+//  Created by Joseph Lyons on 5/9/17.
+//
+//
+
+#include "FrequencyValues.hpp"
+
+void FrequencyValues::calculate(const double &input)
+{
+    tempo = input;
+    
+    // quarterNormal value must be calculated first, since other values depend on it
+    quarterNormal                    = 60000 / tempo;
+    
+    wholeNormal                      = quarterNormal * 4;
+    wholeDotted                      = wholeNormal * dotted;
+    wholeTriplet                     = wholeNormal * triplet;
+    
+    halfNormal                       = quarterNormal * 2;
+    halfDotted                       = halfNormal * dotted;
+    halfTriplet                      = halfNormal * triplet;
+    
+    // quarterNormal
+    quarterDotted                    = quarterNormal * dotted;
+    quarterTriplet                   = quarterNormal * triplet;
+    
+    eighthNormal                     = quarterNormal / 2;
+    eighthDotted                     = eighthNormal * dotted;
+    eighthTriplet                    = eighthNormal * triplet;
+    
+    sixteenthNormal                  = quarterNormal / 4;
+    sixteenthDotted                  = sixteenthNormal * dotted;
+    sixteenthTriplet                 = sixteenthNormal * triplet;
+    
+    thirtySecondNormal               = quarterNormal / 8;
+    thirtySecondDotted               = thirtySecondNormal * dotted;
+    thirtySecondTriplet              = thirtySecondNormal * triplet;
+    
+    sixtyFourthNormal                = quarterNormal / 16;
+    sixtyFourthDotted                = sixtyFourthNormal * dotted;
+    sixtyFourthTriplet               = sixtyFourthNormal * triplet;
+    
+    oneHundredAndTwentyEighthNormal  = quarterNormal / 32;
+    oneHundredAndTwentyEighthDotted  = oneHundredAndTwentyEighthNormal * dotted;
+    oneHundredAndTwentyEighthTriplet = oneHundredAndTwentyEighthNormal * triplet;
+}
