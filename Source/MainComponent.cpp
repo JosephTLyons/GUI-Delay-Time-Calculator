4f72cf79d4ea94af0f12e4dd8c1c18e6be235f48
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-09 20:58:54 -0500

    Using ints to make values more descriptive

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -971,8 +971,10 @@ void MainComponent::buildInformationWindow()
         basicWindow->setUsingNativeTitleBar (true);
         basicWindow->setContentOwned (new InformationComponent(), true);
 
-        basicWindow->setCentrePosition (getScreenPosition().x + (getWidth() / 2),
-                                        getScreenPosition().y + (getHeight() / 2));
+        int centerOfThisComponentX = getScreenPosition().x + (getWidth() / 2);
+        int centerOfThisComponentY = getScreenPosition().y + (getHeight() / 2);
+        basicWindow->setCentrePosition (centerOfThisComponentX, centerOfThisComponentY);
+        
         basicWindow->setVisible (true);
     }
 
