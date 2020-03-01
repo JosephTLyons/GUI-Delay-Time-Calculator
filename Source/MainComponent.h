5d7b71ec2ed6dcd20e40365b024c1e7ef704d6e7
Joseph T Lyons <JosephTLyons@gmail.com>
2020-02-29 22:37:10 -0500

    Upgrade Projucer 5.4.7

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -7,7 +7,7 @@
   the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
   and re-saved.
 
-  Created with Projucer version: 5.4.5
+  Created with Projucer version: 5.4.7
 
   ------------------------------------------------------------------------------
 
@@ -45,7 +45,7 @@ class MainComponent  : public Component,
 public:
     //==============================================================================
     MainComponent ();
-    ~MainComponent();
+    ~MainComponent() override;
 
     //==============================================================================
     //[UserMethods]     -- You can add your own custom methods in this section.
